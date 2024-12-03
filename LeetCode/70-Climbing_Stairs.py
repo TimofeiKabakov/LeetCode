3e8038dc505f15389c6ef542470c32b00078028b
Timofei Kabakov <timofeikabakov22@gmail.com>
2024-12-03 17:41:04 -0600

    feat: 12-03-2024

--- LeetCode/70-Climbing_Stairs.py
+++ LeetCode/70-Climbing_Stairs.py
@@ -1,13 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-            if n == 1:
-                return 1
-
-            steps = [0] * n
-            steps[0] = 1
-            steps[1] = 2
-
-            for i in range(2, n):
-                steps[i] = steps[i - 1] + steps[i - 2]
-
-            return steps[n - 1]
+        dp = [0] * n
+        if n >= 1:
+            dp[0] = 1
+        if n >= 2:
+            dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[-1]
+    
+# Time Complexity: O(n)
+# Space Complexity: O(n)
