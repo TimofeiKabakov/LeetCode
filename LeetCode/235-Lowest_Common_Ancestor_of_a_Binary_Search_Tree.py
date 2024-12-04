3d4fcb0c5bcb560d7b0368754c2eaf5c34210740
Timofei Kabakov <timofeikabakov22@gmail.com>
2024-12-03 23:41:27 -0600

    12-03-2024

--- LeetCode/235-Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ LeetCode/235-Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -1,58 +1,19 @@
-# Working solution but overcomplicated
 class Solution:
-    def __init__(self):
-        self.counter = 0
-        self.map = {}
-
-    def dfs(self, node):
-        if not node:
-            return 0
-        self.map[self.counter] = node.val
-        node.val = self.counter
-        self.counter += 1
-        return self.dfs(node.left) + self.dfs(node.right) + 1 
-
-    def find_parents(self, node, parents):
-        if not node:
-            return
-        
-        if node.left:
-            parents[node.left.val] = node
-        if node.right:
-            parents[node.right.val] = node
-        
-        self.find_parents(node.left, parents)
-        self.find_parents(node.right, parents)
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        res = 0
-        number_of_elements = self.dfs(root)
-        tree_parents = [0] * number_of_elements
-        tree_parents[0] = root
-        self.find_parents(root, tree_parents)
+        def dfs(node):
+            if not node:
+                return None
 
-        set_p = set()
-        set_q = set()
-        
-        while(p != q):
-            set_p.add(p.val)
-            set_q.add(q.val)
-            print("p:", p.val, "q:", q.val)
-            if p != root:
-                p = tree_parents[p.val]
-            if q != root:
-                q = tree_parents[q.val]
-            
-            if q.val in set_p:
-                q.val = self.map.get(q.val)
-                return q
-            if p.val in set_q:
-                p.val = self.map.get(p.val)
+            if (p.val < node.val and q.val > node.val) or (q.val < node.val and p.val > node.val):
+                return node
+            elif p.val == node.val:
                 return p
+            elif q.val == node.val:
+                return q
 
-        p.val = self.map.get(p.val)
-        return p
+            return dfs(node.left) if p.val < node.val and q.val < node.val else dfs(node.right)
 
+        return dfs(root)
 
-# Time Complexity: O(n)
-# Space Complexity: O(n)
+# Time Complexity: O(log n)
+# Space Complexity: O(1)
