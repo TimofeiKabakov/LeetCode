4c5ebb349643d6418f4fc21e8f9d04ba3a925c94
Timofei Kabakov <timofeikabakov22@gmail.com>
2024-12-05 22:39:55 -0600

    12-05-2024

--- LeetCode/121-Best_Time_to_Buy_and_Sell_Stock.py
+++ LeetCode/121-Best_Time_to_Buy_and_Sell_Stock.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        min_price = 1000
         max_profit = 0
-        minimumPrice = 10001
 
         for p in prices:
-            diff = p - minimumPrice
-            if max_profit < diff:
-                max_profit = diff
-
-            if minimumPrice > p:
-                minimumPrice = p
+            if p < min_price:
+                min_price = p
+            else:
+                profit = p - min_price
+                if max_profit < profit:
+                    max_profit = profit
 
         return max_profit
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
